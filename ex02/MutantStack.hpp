2aa8d1a8e52ebd90855b8f2b42c49a9b70920d79
Lucas Maume <lmaume@k0r4p9.42mulhouse.fr>
2025-08-05 09:08:56 +0200

    ex02 MutantStack done

---
+++ ex02/MutantStack.hpp
@@ -0,0 +1,21 @@
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+#include <stack>
+#include <deque>
+#include <iterator>
+#include <iostream>
+
+template <class T, class Container = std::deque<T> >
+class MutantStack : public std::stack<T>
+{
+	private:
+	public:
+	MutantStack(){}
+	~MutantStack(){}
+	typedef typename Container::iterator iterator;
+	iterator	begin(){return this->c.begin();}
+	iterator	end(){return this->c.end();}
+};
+
+#endif
